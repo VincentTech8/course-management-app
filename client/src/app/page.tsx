cc93ee2e404c8136879c2b3ae279cfd99a39acdc
vinctechgeek <toxicgeek8@gmail.com>
2024-12-16 01:13:28 +1100

    Add

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,3 +1,13 @@
+import NonDashboardNavbar from "@/components/NonDashboardNavbar";
+import Landing from "@/app/(nondashboard)/landing/page";
+
 export default function Home() {
-  return <div className="">Hi</div>;
+  return (
+    <div className="nondashboard-layout">
+      <NonDashboardNavbar />
+      <main className="nondashboard-layout__main">
+        <Landing />
+      </main>
+    </div>
+  );
 }
---
+++ client/src/components/NonDashboardNavbar.tsx
@@ -0,0 +1,46 @@
+import { Bell, BookOpen } from "lucide-react";
+import Link from "next/link";
+import React from "react";
+
+const NonDashboardNavbar = () => {
+  return (
+    <nav className="nondashboard-navbar">
+      <div className="nondashboard-navbar__container">
+        {/* Left Side - Navbar */}
+        <div className="nondashboard-navbar__search">
+          <Link href="/" className="nondashboard-navbar__brand" scroll={false}>
+            Vdemy
+          </Link>
+          <div className="flex items-center gap-4">
+            <div className="relative group">
+              <Link
+                href="/search"
+                className="nondashboard-navbar__search-input"
+                scroll={false}
+              >
+                <span className="hidden sm:inline">Search Courses</span>
+                <span className="sm:hidden">Search</span>
+              </Link>
+              <BookOpen
+                className="nondashboard-navbar__search-icon"
+                size={18}
+              />
+            </div>
+          </div>
+        </div>
+        {/* Right Side - Navbar */}
+        <div className="nondashboard-navbar__actions">
+          <button className="nondashboard-navbar__notification-button">
+            <span className="nondashboard-navbar__notification-indicator"></span>
+            <Bell className="nondashboard-navbar__notification-icon" />
+          </button>
+
+          {/* Sign In Buttons */}
+
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default NonDashboardNavbar;
