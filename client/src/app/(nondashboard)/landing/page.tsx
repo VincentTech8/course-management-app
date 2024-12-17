1e0978d40c16ec242dd3375510c8d1047aa0e1c7
vinctechgeek <toxicgeek8@gmail.com>
2024-12-17 13:24:46 +1100

    Add

--- client/src/app/(nondashboard)/landing/page.tsx
+++ client/src/app/(nondashboard)/landing/page.tsx
@@ -6,6 +6,7 @@ import Link from "next/link";
 import Image from "next/image";
 import { useCarousel } from "@/hooks/useCarousel";
 import { Skeleton } from "@/components/ui/skeleton";
+import { useGetCoursesQuery } from "@/state/api";
 
 const LoadingSkeleton = () => {
   return (
@@ -42,6 +43,11 @@ const LoadingSkeleton = () => {
 
 const Landing = () => {
   const currentImage = useCarousel({ totalImages: 3 });
+  const { data: courses, isLoading, isError } = useGetCoursesQuery({});
+  console.log("courses:", courses);
+
+  // if (isLoading) return <LoadingSkeleton />;
+  // if (isError || !courses) return <div>Error fetching courses</div>;
 
   return (
     <motion.div
