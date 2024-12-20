02e1422568bc06d64421251620cfefd51f9253dd
vinctechgeek <toxicgeek8@gmail.com>
2024-12-20 12:37:09 +1100

    Add

--- client/src/components/SignIn.tsx
+++ client/src/components/SignIn.tsx
@@ -1,8 +1,32 @@
-import { SignIn } from "@clerk/nextjs";
+"use client";
+
+import { SignIn, useUser } from "@clerk/nextjs";
 import React from "react";
 import { dark } from "@clerk/themes";
+import { useSearchParams } from "next/navigation";
 
 const SignInComponent = () => {
+  const { user } = useUser();
+  const searchParams = useSearchParams();
+  const isCheckoutPage = searchParams.get("showSignUp") !== null;
+  const courseId = searchParams.get("id");
+
+  const signUpUrl = isCheckoutPage
+    ? `/checkout?step=1&id=${courseId}&showSignUp=true`
+    : "/signup";
+
+  const getRedirectUrl = () => {
+    if (isCheckoutPage) {
+      return `/checkout?step=2&id=${courseId}&showSignUp=true`;
+    }
+
+    const userType = user?.publicMetadata?.userType as string;
+    if (userType === "teacher") {
+      return "/teacher/courses";
+    }
+    return "/user/courses";
+  };
+
   return (
     <SignIn
       appearance={{
@@ -25,6 +49,10 @@ const SignInComponent = () => {
           footerActionLink: "text-primary-750 hover:text-primary-600",
         },
       }}
+      signUpUrl={signUpUrl}
+      forceRedirectUrl={getRedirectUrl()}
+      routing="hash"
+      afterSignOutUrl="/"
     />
   );
 };
