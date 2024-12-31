bc7ceaa13ce982756e4b947710bdfbc5cf60580d
vinctechgeek <toxicgeek8@gmail.com>
2024-12-31 17:24:38 +1100

    Add courseSidebar

--- client/src/app/(dashboard)/layout.tsx
+++ client/src/app/(dashboard)/layout.tsx
@@ -7,7 +7,8 @@ import { SidebarProvider } from "@/components/ui/sidebar";
 import { cn } from "@/lib/utils";
 import { useUser } from "@clerk/nextjs";
 import { usePathname } from "next/navigation";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import ChaptersSidebar from "./user/courses/[courseId]/ChaptersSidebar";
 
 export default function DashboardLayout({
   children,
@@ -17,8 +18,18 @@ export default function DashboardLayout({
   const pathname = usePathname();
   const [courseId, setCourseId] = useState<string | null>(null);
   const { user, isLoaded } = useUser();
+  const isCoursePage = /^\/user\/courses\/[^\/]+(?:\/chapters\/[^\/]+)?$/.test(
+    pathname
+  );
 
-  //   handle use effect isCoursePage
+  useEffect(() => {
+    if (isCoursePage) {
+      const match = pathname.match(/\/user\/courses\/([^\/]+)/);
+      setCourseId(match ? match[1] : null);
+    } else {
+      setCourseId(null);
+    }
+  }, [isCoursePage, pathname]);
 
   if (!isLoaded) return <Loading />;
   if (!user) return <div>Please sign in to access this page.</div>;
@@ -26,12 +37,17 @@ export default function DashboardLayout({
   return (
     <SidebarProvider>
       <div className="dashboard">
-        {/* Sidebar will go here */}
         <AppSidebar />
         <div className="dashboard__content">
-          {/* Chapter sidebar will go here */}
-          <div className={cn("dashboard__main")} style={{ height: "100vh" }}>
-            <Navbar />
+          {courseId && <ChaptersSidebar />}
+          <div
+            className={cn(
+              "dashboard__main",
+              isCoursePage && "dashboard__main--not-course"
+            )}
+            style={{ height: "100vh" }}
+          >
+            <Navbar isCoursePage={isCoursePage} />
             <main className="dashboard__body">{children}</main>
           </div>
         </div>
